eb1814b27f2dad7ee6d0b5fa39d4b460a6dcd6dc
Saroj Roul <sarojroul300@gmail.com>
2024-07-21 12:13:04 +0530

    done

---
+++ CLASS - 1/01 - variable.js
@@ -0,0 +1,42 @@
+// decalre a variable
+
+// Number :
+
+let num = 100;
+console.log(num); // to print the output
+
+let Num = 50;
+console.log(Num);
+
+// space dosn't allow while declaring variable
+// let second num = 1000;
+
+let second_num = 1000; // second_num(snakeCase)
+console.log(second_num);
+
+let secondNum = 1000; // secondNum(camelCase)
+console.log(secondNum);
+
+let decimal_num = 15.35;
+console.log(decimal_num);
+
+console.log(typeof decimal_num);
+
+// Task:
+// task1: Declare a number variable and assign a value to it.
+// task2: Declare different variables of numbers and assign different values to perform mathematical operation.
+// eg: addition , subtraction , multiplication , division and modulus.
+
+// Examples:
+// let num1 = 12;
+// let num2 = 13;
+// let addition = num1 + num2;
+// console.log(addition); // 25
+
+// String :
+
+// String is a collection of charcters.
+// String should be in " ... "
+
+let name = "Pramod";
+console.log("My name is:", name); // Saroj
---
+++ CLASS - 1/04 - string.js
@@ -0,0 +1,42 @@
+// String
+let name1 = "Pramod";
+let name2 = "Kumar";
+let name3 = "Jena";
+
+let sentence = "He is a good boy"; // 16 char
+
+// empty string "" " "
+// console.log(name1 + " " + name2 + " " + name3 + "\n" + sentence);
+
+// charAt() : function
+// It returns the character at the specified index in a string.
+
+console.log(sentence.charAt(10)); // e
+
+// length
+console.log(sentence.length); // 16
+
+let lengthCheck =
+  "   Lorem  ipsum dolor sit amet consectetur adipisicing elit. Doloribus at rerum ea minus nulla itaque ab vero ad asperiores exercitationem    ";
+console.log(lengthCheck.length); // 134
+
+// split()
+// console.log(sentence.split(" "));
+console.log(sentence.toUpperCase()); // HE IS A GOOD BOY
+console.log(lengthCheck);
+console.log(lengthCheck.trim());
+console.log(lengthCheck);
+
+//todo Task: 1
+// Use all the string methods to demonstrate all the properties with examples.
+
+// Example:
+// let sentence = "He is a good boy"; // 16 char
+
+// console.log(sentence.includes("is"));
+
+//todo Task: 2
+// Declare a sentence and include your name in that and check wheather your name is present or not using `include` method.
+
+// Example:
+// let sentence = Lorem ipsum dolor sit amet pramod consectetur adipisicing elit. Perferendis, nihil.
